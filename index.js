a2dde03b0cdae43e0668bd49efe1112b3ac3a08f
Steve Schmechel <sschmechel@MSI.local>
2017-03-12 16:17:28 -0500

    Initial remote post setup using request library.

---
+++ index.js
@@ -0,0 +1,32 @@
+exports.helloWorld = function helloWorld(req, res) {
+
+    var request = require("request");
+
+    var uri = req.body.cburi;
+    var usr = req.body.cbusr;
+    var pwd = req.body.cbpwd;
+
+    delete req.body.cburi;
+    delete req.body.cbusr;
+    delete req.body.cbpwd;
+
+    request({
+        method: 'POST',
+        uri: uri,
+        json: true,
+        headers: { 'content-type': 'application/json' },
+        body: req.body
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body)
+        }
+        else {
+
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    }).auth(usr, pwd, true);
+
+    res.send(req.body);
+};
